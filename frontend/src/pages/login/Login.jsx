5a263b0d3c4afe99f3096d68fb0f7b3de38a5a0a
https://github.com/igorfonseca05 <78857547+igorfonseca05@users.noreply.github.com>
2025-06-02 15:34:37 -0300

    implementing client_side login form

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -4,18 +4,57 @@ import './Login.css';
 export default function SignIn() {
     const [form, setForm] = useState({ email: '', password: '' });
 
-    const handleChange = e => {
-        setForm({ ...form, [e.target.name]: e.target.value });
+    const handleChange = (e) => {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value
+        });
     };
+
+    async function handleForm(e) {
+        e.preventDefault()
+
+        try {
+
+            const res = await fetch('http://localhost:5000/users/login', {
+                method: 'POST',
+                headers: {
+                    "Content-type": 'application/json'
+                },
+                body: JSON.stringify(form)
+            })
+
+            if (!res.ok) {
+                throw new Error('Erro ao obter dados do usuário')
+            }
+
+            const userData = await res.json()
+
+            console.log(userData)
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
 
 
     return (
         <div className="auth-container">
             <h2>Sign In</h2>
-            <form className="auth-form">
-                <input name="email" type="email" placeholder="Email" required />
-                <input name="password" type="password" placeholder="Password" required />
+            <form onSubmit={handleForm} className="auth-form">
+                <input
+                    name="email"
+                    type="email"
+                    placeholder="Email"
+                    required
+                    onChange={(e) => handleChange(e)} />
+                <input
+                    name="password"
+                    type="password"
+                    placeholder="Password"
+                    required
+                    onChange={(e) => handleChange(e)} />
                 <button type="submit">Login</button>
             </form>
         </div>
